02c058d167f8bbdf99be72a18ba294dfb37c100a
whearn <wchearn00@gmail.com>
2017-07-12 12:15:21 -0400

    Added DB Variables

--- server/config/db.js
+++ server/config/db.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 
 var pool = mysql.createPool({
     connectionLimit: 10,
-    host: 'localhost',
-    user: 'angularBlogUser',
-    password: 'gurmagangular',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     database: 'AngularBlog'
 });
 exports.pool = pool;
